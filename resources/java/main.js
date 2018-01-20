29115b811d78943fd7e941d64b1a347dee75ebb2
Emmz <33159350+MsEmmz@users.noreply.github.com>
2018-01-20 11:03:46 -0500

    added slick but carousel not working

---
+++ resources/java/main.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+  $('.carousel').slick({
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 4000,
+    arrows: false,
+    draggable: false,
+    pauseOnFocus: false,
+    pauseOnHover: false
+  });
+
+
+});
