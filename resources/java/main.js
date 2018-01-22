97d657b3a95743952f6eb7bd4f5254e2d68edcae
Emmz <33159350+MsEmmz@users.noreply.github.com>
2018-01-22 09:13:51 -0500

    added js

--- resources/java/main.js
+++ resources/java/main.js
@@ -1,3 +1,5 @@
+
+
 $(document).ready(function(){
   $('.carousel').slick({
     slidesToScroll: 1,
@@ -10,12 +12,19 @@ $(document).ready(function(){
   });
 
 
-     var a = $(".service");
-     a.click(function()
-     {
+    var lastScrollTop = 0;
+    $(window).scroll(function(){
+      var scrollTop = $(this).scrollTop();
+       if (scrollTop - lastScrollTop > 50){
+        var navHeight = $('.navbar').css('height');
+        $('.navbar').animate({top: '-' + navHeight}, 150);
+        userScrollLength = scrollTop;
+      } else if (lastScrollTop - scrollTop > 50) {
+        $('.navbar').animate({top: '0px'}, 150);
+        lastScrollTop = scrollTop;
+      }
+    });
 
-         return false;
 
-     });
-  });
+
 });
